6b7dd72165af6524d6d1bf9ff3011323539b7b7b
liansun96 <liansun0203@gmail.com>
2025-01-04 23:55:16 +0630

    update

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,12 @@
+const Product = require('../models/product')
+
 const getAllProducts = (req , res) => {
     res.status(200).json({msg : 'Get All Products'})
 }
 
-const getAllProductsStatic = (req , res) => {
-    res.status(200).json({msg : 'Get All Products Static'})
+const getAllProductsStatic = async(req , res) => {
+    const products = await Product.find({})
+    res.status(200).json({products})
 }
 
 module.exports = {getAllProducts , getAllProductsStatic}
