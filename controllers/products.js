8383f84649d2e988a9657833bb33446158c2856e
liansun96 <liansun0203@gmail.com>
2025-01-05 18:58:52 +0630

    update product controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,23 @@
 const Product = require('../models/product')
 
-const getAllProducts = (req , res) => {
-    res.status(200).json({msg : 'Get All Products'})
-}
-
 const getAllProductsStatic = async(req , res) => {
-    const products = await Product.find({})
-    res.status(200).json({products})
+    const products = await Product.find({featured : true})
+    res.status(200).json({nbHits : products.length , products})
+}  
+
+const getAllProducts = async (req , res) => {
+    const {featured} = req.query
+
+    const queryObject = {}
+    
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    console.log(queryObject);    
+
+    const products = await Product.find(queryObject)
+    // const products = await Product.find(req.query)
+    res.status(200).json({nbHits : products.length , products})
 }
 
 module.exports = {getAllProducts , getAllProductsStatic}
