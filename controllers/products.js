4dd8b59440d1033a2103761a9e151bdbb1b09524
liansun96 <liansun0203@gmail.com>
2025-01-06 22:57:09 +0630

    update product controller

--- controllers/products.js
+++ controllers/products.js
@@ -6,13 +6,22 @@ const getAllProductsStatic = async(req , res) => {
 }  
 
 const getAllProducts = async (req , res) => {
-    const {featured} = req.query
+    const {featured , company , name} = req.query
 
     const queryObject = {}
     
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
     }
+
+    if(company){
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+
     console.log(queryObject);    
 
     const products = await Product.find(queryObject)
