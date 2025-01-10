116d799e8408830e9c0c445070f51236905a253f
liansun96 <liansun0203@gmail.com>
2025-01-10 18:39:44 +0630

    apply numeric filter

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async(req , res) => {
 }  
 
 const getAllProducts = async (req , res) => {
-    const {featured , company , name , sort , fields} = req.query
+    const {featured , company , name , sort , fields , numericFilters} = req.query
 
     const queryObject = {}
     
@@ -22,6 +22,31 @@ const getAllProducts = async (req , res) => {
         queryObject.name = {$regex : name , $options : 'i'};
     }
 
+    if(numericFilters){
+        const operatorMap = {
+            '>' : '$gt',
+            '>=' : '$gte',
+            '=' : '$eq',
+            '<' : '$lt',
+            '<=' : '$lte'
+        }
+
+        const regEx = /\b(>|>=|=|<|<=)\b/g
+
+        let filters = numericFilters.replace(regEx , (match) => `-${operatorMap[match]}-`)
+        // console.log(numericFilters , filters);
+
+        const options = ['price' , 'rating']
+        filters = filters.split(',').forEach((item) => {
+            // console.log(item);
+            
+            const [field , operator , value ] = item.split('-');
+            if(options.includes(field)){
+                queryObject[field] = {[operator] : Number(value)}
+            }
+            console.log(queryObject);            
+        })        
+    }    
     
     let result = Product.find(queryObject)
     
@@ -41,15 +66,15 @@ const getAllProducts = async (req , res) => {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
-
-  result = result.skip(skip).limit(limit);
-
-  const products = await result;
     
-    console.log(queryObject , req.query , page , skip);     
+    result = result.skip(skip).limit(limit);
+    
+    const products = await result;
+    
+    // console.log(queryObject , req.query , page , skip);     
     // const products = await Product.find(req.query)
     res.status(200).json({nbHits : products.length , products})
 }
 
-
+    
 module.exports = {getAllProducts , getAllProductsStatic}
