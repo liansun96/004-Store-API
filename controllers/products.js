eca3bc105bd49bd8d9e02577d8aa2c538aca84f9
liansun96 <liansun0203@gmail.com>
2025-01-12 16:27:25 +0630

    check numericFilters

--- controllers/products.js
+++ controllers/products.js
@@ -30,22 +30,29 @@ const getAllProducts = async (req , res) => {
             '<' : '$lt',
             '<=' : '$lte'
         }
-
         const regEx = /\b(>|>=|=|<|<=)\b/g
 
-        let filters = numericFilters.replace(regEx , (match) => `-${operatorMap[match]}-`)
-        // console.log(numericFilters , filters);
+        // let filters = numericFilters.replace(regEx , (match) => `-${operatorMap[match]}-`)
+
+        // const options = ['price' , 'rating']
+        // filters = filters.split(',').map((item) => {
+        //     console.log(item);              
+        //     const [field , operator , value] = item.split('-')
+        //     if(options.includes(field)){
+        //         queryObject[field] = {[operator] : Number(value)}
+        //     }
+        // })
+
+      let filters = numericFilters.replace(regEx , (match) => `-${operatorMap[match]}-`)
+
+      const options = ['price' , 'rating']
+      filters = filters.split(',').map((item) => {
+        const [field , operator , value ] = item.split('-')
+        if(options.includes(field)){
+            queryObject[field] = {[operator] : Number(value)}
+        }
+      })
         
-        const options = ['price' , 'rating']
-        filters = filters.split(',').forEach((item) => {
-            // console.log(item);
-            
-            const [field , operator , value ] = item.split('-');
-            if(options.includes(field)){
-                queryObject[field] = {[operator] : Number(value)}
-            }
-            console.log(queryObject);            
-        })        
     }    
     
     let result = Product.find(queryObject)
